2a60a2404fa5b8a9a0295592a8837b5acf2d5319
James Donlan <jacketlizard@live.com>
2019-03-24 01:30:54 -0700

    travelling cursor radius

--- main.lua
+++ main.lua
@@ -15,8 +15,24 @@ function love.update(dt)
   cos = math.cos(angle)
   sin = math.sin(angle)
   
-  circle.x = circle.x + circle.speed * cos * dt
-  circle.y = circle.y + circle.speed * sin * dt
+  local distance = getDistance(circle.x, circle.y, mouse_x, mouse_y)
+  
+  if distance < 400 then
+    circle.x = circle.x + circle.speed * cos * (distance/100) * dt
+    circle.y = circle.y + circle.speed * sin * (distance/100) * dt
+  end
+end
+
+function getDistance(x1, y1, x2, y2)
+  local horizontal_distance = x1 - x2
+  local vertical_distance = y1 - y2
+  
+  local a = horizontal_distance ^2
+  local b = vertical_distance ^2
+  
+  local c = a + b
+  local distance = math.sqrt(c)
+  return distance
 end
 
 function love.draw()
@@ -24,8 +40,10 @@ function love.draw()
   
   love.graphics.print("angle: " .. angle, 10, 10)
   
-  love.graphics.line(circle.x, circle.y, mouse_x, circle.y)
-  love.graphics.line(circle.x, circle.y, circle.x, mouse_y)
-  
   love.graphics.line(circle.x, circle.y, mouse_x, mouse_y)
+  love.graphics.line(circle.x, circle.y, mouse_x, circle.y)
+  love.graphics.line(mouse_x, mouse_y, mouse_x, circle.y)
+  
+  local distance = getDistance(circle.x, circle.y, mouse_x, mouse_y)
+  love.graphics.circle("line", circle.x, circle.y, distance)
 end
